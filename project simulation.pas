5125eab1db23511538f1121b9a6a4e5433e510fd
Roman-pl <romanplohotnuk1@gmail.com>
2021-10-17 13:46:10 +0300

    Possibility true version + time

--- project simulation.pas
+++ project simulation.pas
@@ -56,234 +56,26 @@ begin
   k := 0;
   i := 0;
   n := random(1, 10);
-  if PosPredator = 0.0 then v := 0
-  else 
-  if PosPredator = 0.1 then v := random(1, 10)
-    else 
-  if PosPredator = 0.2 then v := random(2, 10)
-        else 
-  if PosPredator = 0.3 then v := random(3, 10)
-          else 
-  if PosPredator = 0.4 then v := random(4, 10)
-            else
-  if PosPredator = 0.5 then v := random(5, 10)
-               else 
-  if PosPredator = 0.6 then v := random(6, 10)
-                 else 
-  if PosPredator = 0.7 then v := random(7, 10)
-                   else 
-  if PosPredator = 0.8 then v := random(8, 10)
-                    else 
-  if PosPredator = 0.9 then v := random(9, 10)
-                      else 
-  if PosPredator = 1.0 then v := random(10, 10);
-  
-  if PosPrey = 0.0 then n := 0
-  else 
-  if PosPrey = 0.1 then
-  begin
-    if (life[x][y] = 1) and n = 1  then // если да то где размножится 
-      if b = 0 then Next[x][y + 1] := 1
-        else
-      if b = 2 then Next[x + 1][y] := 1
-          else
-      if b = 1 then Next[x + 1][y + 1] := 1
-            else
-      if b = 3 then Next[x + 1][y + 1] := 1
-              else
-      if b = 4 then Next[x][y - 1] := 1
-                else
-      if b = 5 then Next[x - 1][y - 1] := 1
-                  else
-      if b = 6 then Next[x - 1][y] := 1
-                    else
-      if b = 7 then Next[x - 1][y + 1] := 1;
-  end
-    else 
-  if PosPrey = 0.2 then
-  begin
-    if (life[x][y] = 1) and (n = 1) or (n = 2)  then // если да то где размножится 
-      if b = 0 then Next[x][y + 1] := 1
-        else
-      if b = 2 then Next[x + 1][y] := 1
-          else
-      if b = 1 then Next[x + 1][y + 1] := 1
-            else
-      if b = 3 then Next[x + 1][y + 1] := 1
-              else
-      if b = 4 then Next[x][y - 1] := 1
-                else
-      if b = 5 then Next[x - 1][y - 1] := 1
-                  else
-      if b = 6 then Next[x - 1][y] := 1
-                    else
-      if b = 7 then Next[x - 1][y + 1] := 1;
-  end
-        else 
-  if PosPrey = 0.3 then
-  begin
-    if (life[x][y] = 1) and (n = 1) or (n = 2) or (n = 3)  then // если да то где размножится 
-      if b = 0 then Next[x][y + 1] := 1
-        else
-      if b = 2 then Next[x + 1][y] := 1
-          else
-      if b = 1 then Next[x + 1][y + 1] := 1
-            else
-      if b = 3 then Next[x + 1][y + 1] := 1
-              else
-      if b = 4 then Next[x][y - 1] := 1
-                else
-      if b = 5 then Next[x - 1][y - 1] := 1
-                  else
-      if b = 6 then Next[x - 1][y] := 1
-                    else
-      if b = 7 then Next[x - 1][y + 1] := 1;
-  end 
-          else 
-  if PosPrey = 0.4 then
-  begin
-    if (life[x][y] = 1) and (n = 1) or (n = 2) or (n = 3) or (n = 4)  then // если да то где размножится 
-      if b = 0 then Next[x][y + 1] := 1
-        else
-      if b = 2 then Next[x + 1][y] := 1
-          else
-      if b = 1 then Next[x + 1][y + 1] := 1
-            else
-      if b = 3 then Next[x + 1][y + 1] := 1
-              else
-      if b = 4 then Next[x][y - 1] := 1
-                else
-      if b = 5 then Next[x - 1][y - 1] := 1
-                  else
-      if b = 6 then Next[x - 1][y] := 1
-                    else
-      if b = 7 then Next[x - 1][y + 1] := 1;
-  end 
-            else
-  if PosPrey = 0.5 then
-  begin
-    if (life[x][y] = 1) and (n = 1) or (n = 2) or (n = 3) or (n = 4) or (n = 5) then // если да то где размножится 
-      if b = 0 then Next[x][y + 1] := 1
-        else
-      if b = 2 then Next[x + 1][y] := 1
-          else
-      if b = 1 then Next[x + 1][y + 1] := 1
-            else
-      if b = 3 then Next[x + 1][y + 1] := 1
-              else
-      if b = 4 then Next[x][y - 1] := 1
-                else
-      if b = 5 then Next[x - 1][y - 1] := 1
-                  else
-      if b = 6 then Next[x - 1][y] := 1
-                    else
-      if b = 7 then Next[x - 1][y + 1] := 1;
-  end 
-               else 
-  if PosPrey = 0.6 then 
-  begin
-    if (life[x][y] = 1) and (n = 1) or (n = 2) or (n = 3) or (n = 4) or (n = 5) or (n = 6) then // если да то где размножится 
-      if b = 0 then Next[x][y + 1] := 1
-        else
-      if b = 2 then Next[x + 1][y] := 1
-          else
-      if b = 1 then Next[x + 1][y + 1] := 1
-            else
-      if b = 3 then Next[x + 1][y + 1] := 1
-              else
-      if b = 4 then Next[x][y - 1] := 1
-                else
-      if b = 5 then Next[x - 1][y - 1] := 1
-                  else
-      if b = 6 then Next[x - 1][y] := 1
-                    else
-      if b = 7 then Next[x - 1][y + 1] := 1;
-  end
-                 else 
-  if PosPrey = 0.7 then
-  begin
-    if (life[x][y] = 1) and (n = 1) or (n = 2) or (n = 3) or (n = 4) or (n = 5) or (n = 6) or (n = 7) then // если да то где размножится 
-      if b = 0 then Next[x][y + 1] := 1
-        else
-      if b = 2 then Next[x + 1][y] := 1
-          else
-      if b = 1 then Next[x + 1][y + 1] := 1
-            else
-      if b = 3 then Next[x + 1][y + 1] := 1
-              else
-      if b = 4 then Next[x][y - 1] := 1
-                else
-      if b = 5 then Next[x - 1][y - 1] := 1
-                  else
-      if b = 6 then Next[x - 1][y] := 1
-                    else
-      if b = 7 then Next[x - 1][y + 1] := 1;
-  end 
-                   else 
-  if PosPrey = 0.8 then
-  begin
-    if (life[x][y] = 1) and (n = 1) or (n = 2) or (n = 3) or (n = 4) or (n = 5) or (n = 6) or (n = 7) or (n = 8) then // если да то где размножится 
-      if b = 0 then Next[x][y + 1] := 1
-        else
-      if b = 2 then Next[x + 1][y] := 1
-          else
-      if b = 1 then Next[x + 1][y + 1] := 1
-            else
-      if b = 3 then Next[x + 1][y + 1] := 1
-              else
-      if b = 4 then Next[x][y - 1] := 1
-                else
-      if b = 5 then Next[x - 1][y - 1] := 1
-                  else
-      if b = 6 then Next[x - 1][y] := 1
-                    else
-      if b = 7 then Next[x - 1][y + 1] := 1;
-  end 
-                    else 
-  if PosPrey = 0.9 then
-  begin
-    if (life[x][y] = 1) and (n = 1) or (n = 2) or (n = 3) or (n = 4) or (n = 5) or (n = 6) or (n = 7) or (n = 8) or (n = 9) then // если да то где размножится 
-      if b = 0 then Next[x][y + 1] := 1
-        else
-      if b = 2 then Next[x + 1][y] := 1
-          else
-      if b = 1 then Next[x + 1][y + 1] := 1
-            else
-      if b = 3 then Next[x + 1][y + 1] := 1
-              else
-      if b = 4 then Next[x][y - 1] := 1
-                else
-      if b = 5 then Next[x - 1][y - 1] := 1
-                  else
-      if b = 6 then Next[x - 1][y] := 1
-                    else
-      if b = 7 then Next[x - 1][y + 1] := 1;
-  end
-                      else 
-  if PosPrey = 1.0 then
-  begin
-    if (life[x][y] = 1) and (n = 1) or (n = 2) or (n = 3) or (n = 4) or (n = 5) or (n = 6) or (n = 7) or (n = 8) or (n = 9) or (n = 10) then // если да то где размножится 
-      if b = 0 then Next[x][y + 1] := 1
-        else
-      if b = 2 then Next[x + 1][y] := 1
-          else
-      if b = 1 then Next[x + 1][y + 1] := 1
-            else
-      if b = 3 then Next[x + 1][y + 1] := 1
-              else
-      if b = 4 then Next[x][y - 1] := 1
-                else
-      if b = 5 then Next[x - 1][y - 1] := 1
-                  else
-      if b = 6 then Next[x - 1][y] := 1
-                    else
-      if b = 7 then Next[x - 1][y + 1] := 1; 
-  end;
+  v := random(1, 10);
   b := random(0, 7);
   m := random(0, 7);
   k := random(1, fp);
-  
-  
+  if (life[x][y] = 1)  and (random() <= PosPrey) then
+    if b = 0 then Next[x][y + 1] := 1
+    else
+      if b = 1 then Next[x + 1][y] := 1
+    else
+        if b = 2 then Next[x + 1][y + 1] := 1
+    else
+          if b = 3 then Next[x][y - 1] := 1
+    else
+            if b = 4 then Next[x + 1][y - 1] := 1
+    else
+              if b = 5 then Next[x - 1][y - 1] := 1
+    else
+                if b = 6 then Next[x - 1][y + 1] := 1
+    else
+                  if b = 7 then Next[x - 1][y] := 1;
   
   if Life[x][y] = 2 then // проверяем есть ли хищник в клетке и если есть и рядом есть жертва то он размножается
     for i := 1 to fp do
@@ -348,9 +140,8 @@ begin
                     else
       if Life[x - i][y - i] = 1 then c := c + 1;
     end;
-  
-  if  (Life[x][y] = 2) and (c = 0) and (v < 3) then Next[x][y] := 0;
-  if (Life[x][y] = 2) and (c = 0) and (v > 3) then
+  if (life[x][y] = 2) and (c = 0) and (random() > PosPredator) then Next[x][y] := 0;
+  if (life[x][y] = 2) and (c = 0) and (random() <= PosPredator) then
     if m = 0 then 
     begin
       Next[x][y + k] := 2;
@@ -397,7 +188,56 @@ begin
     begin
       Next[x - k][y + k] := 2;
       Next[x][y] := 0;
-    end;
+    end; 
+  {if  (Life[x][y] = 2) and (c = 0) and (v < 3) then Next[x][y] := 0;
+  if (Life[x][y] = 2) and (c = 0) and (v > 3) then
+  if m = 0 then 
+  begin
+  Next[x][y + k] := 2;
+  Next[x][y] := 0;
+  end
+  else
+  if m = 1 then
+  begin
+  Next[x + k][y + k] := 2;
+  Next[x][y] := 0;
+  end
+  else
+  if m = 2 then 
+  begin
+  Next[x + k][y] := 2;
+  Next[x][y] := 0;
+  end
+  else
+  if m = 3 then
+  begin
+  Next[x + k][y + k] := 2;
+  Next[x][y] := 0;
+  end
+  else
+  if m = 4 then
+  begin
+  Next[x][y - k] := 2;
+  Next[x][y] := 0;
+  end
+  else
+  if m = 5 then
+  begin
+  Next[x - k][y - k] := 2;
+  Next[x][y] := 0;
+  end
+  else
+  if m = 6 then
+  begin
+  Next[x - k][y] := 2;
+  Next[x][y] := 0;
+  end
+  else
+  if m = 7 then
+  begin
+  Next[x - k][y + k] := 2;
+  Next[x][y] := 0;
+  end;}
 end;
 
 procedure step;//процедура перехода от одной итерации к другой
@@ -451,7 +291,7 @@ begin
     Readln(F, s);
     a := s.split('=');
     if a[0] = 'predator"s field of view ' then fp := strtoint(a[1]);
-    if a[0] = 'the posibility of deth of a predator ' then PosPredator := strtofloat(a[1]);
+    if a[0] = 'the posibility of death of a predator ' then PosPredator := strtofloat(a[1]);
     if a[0] = 'the posibility of reproduce of a prey ' then PosPrey := strtofloat(a[1]);
   end;
   CloseFile(f);
@@ -463,9 +303,9 @@ begin
     drf;
     time := time + 1;
     writeln(f1, life);
-    if p = 0 then  halt(0)
   until  0 = p;
   writeln(f1, 'время наблюдения за симуляцией: ', time);
   CloseFile(f1);
+  if p=0 then halt(0);
 end.
 // в следущую неделю увидим : у хищника у видим поле зрения запись в файл (желательно в бинарный) считывание из бин файла и построение графикоф 
